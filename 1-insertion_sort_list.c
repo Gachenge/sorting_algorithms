d2ad2506ba55e642686b083399aaf59c7bf93570
Gachenge <gachenge1@gmail.com>
2023-01-22 14:26:00 +0000

    selection sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,26 +1,35 @@
 #include "sort.h"
 
+/**
+  * sort - insertion sort linked list
+  * @n1: first node
+  * @n2: second node
+  */
+
+void swap(*n1, *n2)
+{
+	listint_t *temp;
+
+	temp = n1->next;
+	n
+
+/**
+  * insertion_sort_list - by insertion sort, sort a linked list
+  * @list: the head of the list
+  */
+
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *ls = *list;
-	listint_t *temp;
-	int i, j, len;
+	listint_t *h, *n1, *n2;
 
-	len = 0;
-	while (ls)
+	for (h = *list; h->next; h = h->next)
 	{
-		ls = ls->next;
-		len++;
-	}
-	for (i = 1; i < len; i++)
-	{
-		temp = ls[i];
-		j = i - 1;
-		while(j > 0 && ls[i] > temp)
+		n1 = h->next;
+		n2 = h->prev;
+		while (n2 && h->n < n2->n)
 		{
-			ls[j + 1] = ls[j];
-			j--;
+			swap(n2, h);
+			print_list(*list);
 		}
-		ls[j + 1] = temp;
 	}
 }
---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * selection_sort - selection sort to sort an array
+ * @array: the array to sort
+ * @size: how big is the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			temp = array[min];
+			array[min] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+	}
+}
