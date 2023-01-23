73e513a6b4cf26f658124f5f74eca45dcd2850ee
Gachenge <gachenge1@gmail.com>
2023-01-23 13:05:15 +0000

    betty correction

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -27,7 +27,7 @@ void shell_sort(int *array, size_t size)
 
 	if (!array || size < 2)
 		return;
-	for (gap = 1; gap < (size/ 3);)
+	for (gap = 1; gap < (size / 3);)
 		gap = gap * 3 + 1;
 	for (; gap >= 1; gap /= 3)
 	{
@@ -36,10 +36,10 @@ void shell_sort(int *array, size_t size)
 			j = i;
 			while (j >= gap && array[j - gap] > array[j])
 			{
-				swap((array + j), array+ (j- gap));
+				swap((array + j), array + (j - gap));
 				j -= gap;
 			}
 		}
-	print_array(array, size);
+		print_array(array, size);
 	}
 }
